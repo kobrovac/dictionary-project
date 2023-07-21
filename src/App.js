50fb70fc568183c6542c3ded8e3bc0d0b62ca3e0
kobrovac <120015883+kobrovac@users.noreply.github.com>
2023-07-21 13:38:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo-h.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid"
+        alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
